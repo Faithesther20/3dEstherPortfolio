136312d4681dc8a22c9358e3c6c5922c0a6adfbb
Esther J. Iyege <faithesther20@gmail.com>
2023-06-23 23:42:11 +0100

    how

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Loader = () => {
+import { Html, useProgress } from '@react-three/drei'
+
+
+const Loader = () => { 
+  const {progress} = useProgress();
+
   return (
-    <div>Loader</div>
+   <Html>
+    <span className='canva-load'>
+      <p
+      style={{
+        fontSize:14,
+        color:'#f1f1f1',
+        fontWeight:800,
+        marginTop:40
+
+      }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </span>
+   </Html>
   )
 }
 
